237833776331f0a431dd659b88c172a2900d6878
Abhinav Mathur <amathur5000@gmail.com>
2017-11-30 13:26:16 -0500

    Tested slots

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <button @click="selectedComponent = 'appQuote'">Quote</button>
+    <button @click="selectedComponent = 'appNew'">New</button>
+    <button @click="selectedComponent = 'appAuthor'">Author</button>
+    <br>
+
+
+    <component :is="selectedComponent">
+      hello world
+    </component>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import Quote from './components/Quote.vue'
+  import New from './components/New.vue'
+  import Author from './components/Author.vue'
+
+  export default {
+    data(){
+      return {
+        selectedComponent: 'appQuote'
+      }
+    },
+    components: {
+      appQuote: Quote,
+      appAuthor: Author,
+      appNew: New
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <slot name="title"></slot>
+    <hr>
+    <slot name="description"></slot>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      quote: String
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid white;
+    box-shadow: 1px 1px 2px black;
+    padding: 30px;
+    margin: auto;
+  }
+</style>
