f6b903dde885445894ec826ddbb3ee691df08cba
Abhinav Mathur <amathur5000@gmail.com>
2017-12-05 15:15:21 -0500

    Added reverse

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,44 @@
 <template>
-
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Filters and Mixins</h1>
+        <input type="text" v-model="text">
+        <p>{{ text | reverseText }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app',
+    data(){
+      return {
+        text: 'hello world',
+        fruits: ['Apple', 'Orange', 'Banana', 'Grape'],
+        text: ''
+      }
+    },
+    filters: {
+      reverseText(value){
+        if(value.length !== 0){
+          let str = value.split('').reverse().join('').toLocaleLowerCase();
+          return str.charAt(0).toUpperCase() + str.slice(1)
+        }
+
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   template: '<App/>',
   components: { App }
-})
+});
